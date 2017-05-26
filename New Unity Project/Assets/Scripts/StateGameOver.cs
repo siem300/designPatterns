3871eff832be98cd31a837cc89e79804e1908155
Siem van der Kluft <svdkluft@gmail.com>
2017-05-26 23:13:52 +0200

    Added some state classes, they don't do anything yet

---
+++ New Unity Project/Assets/Scripts/StateGameOver.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateGameOver : State {
+
+    public void doAction(StateContext stateContext)
+    {
+        
+    }
+    
+}
---
+++ New Unity Project/Assets/Scripts/StateMainMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMainMenu : State {
+
+    public void doAction(StateContext stateContext)
+    {
+        
+    }
+}
