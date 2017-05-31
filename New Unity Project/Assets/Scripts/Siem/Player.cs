defcadbe119affbb5d0cf68614f319076de85c50
Erik Oegema <erikoegema@outlook.com>
2017-05-31 10:40:29 +0200

    shizzle

--- New Unity Project/Assets/Scripts/Siem/Player.cs
+++ New Unity Project/Assets/Scripts/Siem/Player.cs
@@ -1,32 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
-
 public class Player : MonoBehaviour
 {
-
     public int movementSpeed;
     public int jumpForce;
-
     public bool isTouchingGround;
     public Transform playerGroundCheck;
     public Transform playerGroundCheck2;
     public LayerMask groundLayer;
-
     private Vector2 moveDirection;
     private Animator characterAnimator;
-
     public float jumpTime;
     bool jumping = false;
-
     public string horizontalInput;
     public string jumpInput;
     public string UseSkillInput;
-
     public AudioSource audioSource;
     public AudioClip jumpClip, skillClip, deathClip;
-
     ISkill skill;
-
     // Use this for initialization
     void Start()
     {
@@ -35,14 +26,12 @@ public class Player : MonoBehaviour
         jumpInput = "Jump" + this.tag;
         UseSkillInput = "UseSkill" + this.tag;
     }
-
     // Update is called once per frame
     void Update()
     {
         float inputX = Input.GetAxis(horizontalInput);
         moveDirection = new Vector2(inputX * movementSpeed, this.gameObject.GetComponent<Rigidbody2D>().velocity.y);
         skill.UpdateCoolDown();
-
         if (isTouchingGround && Input.GetButton(horizontalInput))
         {
             if (Input.GetAxis(horizontalInput) > 0)
@@ -81,10 +70,7 @@ public class Player : MonoBehaviour
         {
             characterAnimator.Play("idle");
         }
-
         JumpLogic();
-
-
         if (Input.GetButton(UseSkillInput))
         {
             skill.executeSkill();
@@ -93,9 +79,7 @@ public class Player : MonoBehaviour
         {
             skill.usingSkill = false;
         }
-
     }
-
     public virtual void JumpLogic()
     {
         if (isTouchingGround && Input.GetButtonDown(jumpInput))
@@ -104,28 +88,21 @@ public class Player : MonoBehaviour
             audioSource.PlayOneShot(jumpClip);
         }
     }
-
-
-
-
     public virtual void FixedUpdate()
     {
+        JumpLogic();
         isTouchingGround = Physics2D.OverlapArea(playerGroundCheck.transform.position, playerGroundCheck2.transform.position, groundLayer);
         this.gameObject.GetComponent<Rigidbody2D>().velocity = moveDirection;
     }
-
-
     public IEnumerator JumpRoutine()
     {
         this.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         float timer = 0;
-
         while (Input.GetButton(jumpInput) && timer < jumpTime)
         {
             //Calculate how far through the jump we are as a percentage
             //apply the full jump force on the first frame, then apply less force
             //each consecutive frame
-            Debug.Log(jumpInput);
             float proportionCompleted = timer / jumpTime;
             Vector2 thisFrameJumpVector = Vector2.Lerp(Vector2.up * jumpForce, Vector2.zero, proportionCompleted);
             this.gameObject.GetComponent<Rigidbody2D>().AddForce(thisFrameJumpVector);
@@ -133,7 +110,6 @@ public class Player : MonoBehaviour
             yield return null;
         }
     }
-
     void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.tag == "DeathWall")
@@ -142,12 +118,10 @@ public class Player : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
-
     public void setSkill(ISkill iSkill)
     {
         skill = iSkill;
     }
-
     void OnDestroy()
     {
         //Camera.main.GetComponent<ScreenShake>().StartShaking();
